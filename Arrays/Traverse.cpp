f7efe78cfd840f2805b68598f59e191656066f66
ankit0pokhriyal <62211049+ankit0pokhriyal@users.noreply.github.com>
2024-08-06 12:11:39 +0530

    Create Traverse.cpp

---
+++ Arrays/Traverse.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, n;
+    int arr[20];
+    cout << "Enter the size of elements";
+    cin >> n;
+    cout << "Enter the elements:";
+    for (i = 0; i < n; i++)
+    {
+        // Code Here
+    }
+    cout << "To check the random access of an array:";
+    // Code Here to find the 4 th index of an array.
+    
+    cout << "Traversal of array: "<<endl;
+    // Write a loop to traverse all  the elements of an array.
+    // Code Here 
+   
+    
+
+}
