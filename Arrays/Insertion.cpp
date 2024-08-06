3731c6e957b36c7c61d2c04a8ab3a88f195029b4
ankit0pokhriyal <62211049+ankit0pokhriyal@users.noreply.github.com>
2024-08-06 12:20:08 +0530

    Create Insertion.cpp

---
+++ Arrays/Insertion.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, n, num, pos;
+    int arr[20];
+
+
+    cout << "Enter the No. of elements: ";
+    // CODE HERE 
+
+    cout << "Enter the elements of the array:" << endl;
+    // CODE HERE
+
+
+    cout << "Enter the No.  or value to be inserted: ";
+    // CODE HERE
+
+    cout << "Enter the position at which the number has to be inserted: ";
+    // CODE HERE
+
+    // Shift elements to the right to make space for the new element
+    // HINT :: Use for loop for that
+    // CODE HERE
+
+    // Insert the number at the specified position
+    // CODE HERE 
+
+    // Increase the size of the array
+    // CODE HERE
+
+    // Print the array after insertion
+    // CODE HERE
+
+    return 0;
+}
