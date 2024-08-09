2675dc36d650fd04044957eb2db733abb790ee05
ankit0pokhriyal <62211049+ankit0pokhriyal@users.noreply.github.com>
2024-08-09 09:43:57 +0530

    Addition of Deletion and merge

---
+++ Arrays/Deletion.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, n, pos, arr[20];
+    
+    cout << "Enter the No. of elements: ";
+    // CODE HERE
+    
+    cout<< "Enter the elements of an array";
+    // Code to enter the elements of an array
+    // Code HERE
+    cout << "Enter the position at which the number has to be deleted: ";
+    // Code HERE
+
+   // Deleting the element from position pos
+   // CODE HERE
+
+    // Update the size of array after deletion
+    // CODE HERE
+    
+
+    cout << "\nThe array after deletion is:\n";
+    // Code HERE
+
+    return 0;
+}
---
+++ Arrays/Merge.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, n;
+    int arr[20];
+    cout << "Enter the no. of elements";
+    cin >> n;
+    cout<<"enter the elements"<<endl;
+    for (i = 0; i < n; i++)
+    {
+
+
+       
+    }
+    cout << "To check the random access of an array:";
+    cout<<"to check the random access of an element";
+   ;
+    cout << "Traversal of array: "<<endl;
+    // Write a loop to traverse all  the elements of an array.
+     for (i = 0; i < n; i++)
+    {
+  
+        
+    return 0;
+    }
+
+}
