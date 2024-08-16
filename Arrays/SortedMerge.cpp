3d475aeffd15cc9ee9c4f623aadde2a9a47e0c6f
ankit0pokhriyal <62211049+ankit0pokhriyal@users.noreply.github.com>
2024-08-16 09:28:44 +0530

    Update SortedMerge.cpp

--- Arrays/SortedMerge.cpp
+++ Arrays/SortedMerge.cpp
@@ -13,40 +13,26 @@ int main() {
     cout << "Enter the No. of elements for sorted arr2: ";
     cin >> n2;
 
-    // Enter the elements for size  n2
+    // Enter the elements of an array for size  n2
     // CODE HERE
 
-    for(j = 0; j<n2; j++)
-    {
-        cin>>arr2[j];
-    }
 
-    // ADD SIZE
+    // ADD SIZE {HINT : n3 will be n1+n2}
     // CODE HERE
-    n3 = n1+n2;
+    
 
     // Till i2 < n1 and j2 <n2
     // CODE HERE
-    while(i2<n1 && j2<n2)
-    {
+   
         // If arr1[i2] < arr2[j2]  fill arr[z] with arr1[i2] elements
         // Remember to increase respective Indexes.
         // CODE HERE
-        if (arr1[i2]<arr2[j2])
-        {
-            arr[z] = arr1[i2];
-            i2++;
-        }
+       
         // IF arr1[i2]>= arr2[j2] fill arr[z] with arr2[j2] elements
         // Remember to Increase Respective Indexes.
         // CODE HERE
-        else 
-        {
-            arr[z] = arr2[j2];
-            j2++;
-        }
-        z++;
-    }
+      
+    
 
    // IF any one of the Two merging array get completely filled in Merged Array
    // Insert the elements of another array into Merged Array.
