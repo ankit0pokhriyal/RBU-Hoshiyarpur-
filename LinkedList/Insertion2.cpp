56d10defd2935e5cd84ed94b4476a9bfbafa296c
ankit0pokhriyal <62211049+ankit0pokhriyal@users.noreply.github.com>
2024-09-17 18:57:26 +0530

    Create Insertion2.cpp

---
+++ LinkedList/Insertion2.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+struct Node{
+  int data;
+  Node * next;
+  Node(int val): data(val), next(nullptr){};
+};
+Node *start = nullptr;
+
+void traverse(){
+
+}
+
+void insertBeg(int num){
+ 
+}
+void insertEnd(int num){
+
+  }
+
+
+void insertAfter(int num, int val)
+{
+ 
+}
+
+void insertBefore(int num, int val){
+
+}
+
+int main() {
+            
+return 0;
+
+}
